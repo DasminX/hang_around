60c0516fd627a1d455e33852ec74dc9a9dc4df0f
DasminX <wojtek.test99@gmail.com>
2024-07-28 22:03:37 +0200

    chore: update places/finder place model types

--- backend/src/places/finder/place.model.ts
+++ backend/src/places/finder/place.model.ts
@@ -1,16 +1,16 @@
 import { google } from "@googlemaps/places/build/protos/protos";
 
-export type GooglePlace = google.maps.places.v1.IPlace;
+export type PlaceI = google.maps.places.v1.IPlace;
 
 export class Place {
   public name: string;
-  public id: GooglePlace["id"];
-  public location: GooglePlace["location"];
-  public rating: GooglePlace["rating"];
-  public mapsUri: GooglePlace["googleMapsUri"];
+  public id: PlaceI["id"];
+  public location: PlaceI["location"];
+  public rating: PlaceI["rating"];
+  public mapsUri: PlaceI["googleMapsUri"];
   public isAccessible: boolean;
 
-  constructor(_place: GooglePlace) {
+  constructor(_place: PlaceI) {
     this.name = _place.displayName!.text!;
     this.id = _place.id;
     this.location = _place.location;
