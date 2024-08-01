e74c40a5b6a29eea6130f0691984cec62e7eb9bd
DasminX <wojtek.test99@gmail.com>
2024-08-01 21:09:33 +0200

    refactor(healthcheck): change to interfaces

--- backend/src/healthcheck/router.ts
+++ backend/src/healthcheck/router.ts
@@ -2,8 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (_req, res, _next) => {
-  return res.json({ status: "Healthy!" });
-});
+router.get("/", (_req, res, _next) => res.json({ alive: true }));
 
 export default router;
