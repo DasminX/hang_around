c9aa67faace27a59ffe2e79f8c2ed8fb57900bed
DasminX <wojtek.test99@gmail.com>
2024-09-15 10:13:35 +0200

    chore: change lib name

---
+++ libs/hang-around-common/src/data/index.ts
@@ -0,0 +1,8 @@
+//prettier-ignore
+export const TYPE_OF_FOOD_ARRAY = [
+    "pizza", "burger", "asian food", "sushi", "pasta", "hungarian food", "kebab", 
+    "polish food", "czech food", "fish", "mexican food", "indian food", "greek food", 
+    "french food", "italian food", "spanish food", "middle eastern food", "thai food", 
+    "vegan food", "vegetarian food", "bbq", "fast food", "comfort food", "seafood", 
+    "desserts", "breakfast food", "brunch food", "mediterranean food"
+  ] as const;
---
+++ libs/hang-around-common/src/value-objects/index.test.ts
@@ -0,0 +1,71 @@
+import { GeoPoint } from "firebase-admin/firestore";
+import { describe, expect, it } from "vitest";
+
+import { LocationArgs, LocationVO } from ".";
+
+describe("LocationVO", () => {
+  it("should create an instance from an array", () => {
+    const locationArgs: LocationArgs = [40.7128, -74.006];
+    const location = new LocationVO(locationArgs);
+
+    expect(location.lat).toBe(40.7128);
+    expect(location.lng).toBe(-74.006);
+  });
+
+  it("should create an instance from a GeoPoint", () => {
+    const geoPoint = new GeoPoint(40.7128, -74.006);
+    const locationArgs: LocationArgs = geoPoint;
+    const location = new LocationVO(locationArgs);
+
+    expect(location.lat).toBe(geoPoint.latitude);
+    expect(location.lng).toBe(geoPoint.longitude);
+  });
+
+  it("should create an instance from an object", () => {
+    const locationArgs: LocationArgs = { lat: 40.7128, lng: -74.006 };
+    const location = new LocationVO(locationArgs);
+
+    expect(location.lat).toBe(40.7128);
+    expect(location.lng).toBe(-74.006);
+  });
+
+  it("should correctly compare two equal LocationVO instances", () => {
+    const location1 = new LocationVO([40.7128, -74.006]);
+    const location2 = new LocationVO({ lat: 40.7128, lng: -74.006 });
+
+    expect(location1.equals(location2)).toBe(true);
+  });
+
+  it("should correctly compare two unequal LocationVO instances", () => {
+    const location1 = new LocationVO([40.7128, -74.006]);
+    const location2 = new LocationVO({ lat: 34.0522, lng: -118.2437 });
+
+    expect(location1.equals(location2)).toBe(false);
+  });
+
+  it("should correctly convert to GeoPoint", () => {
+    const locationArgs: LocationArgs = { lat: 40.7128, lng: -74.006 };
+    const location = new LocationVO(locationArgs);
+
+    const geoPoint = location.toGeoPoint();
+    expect(geoPoint).toBeInstanceOf(GeoPoint);
+    expect(geoPoint.latitude).toBe(40.7128);
+    expect(geoPoint.longitude).toBe(-74.006);
+  });
+
+  it("should correctly convert to tuple", () => {
+    const locationArgs: LocationArgs = { lat: 40.7128, lng: -74.006 };
+    const location = new LocationVO(locationArgs);
+
+    const tuple = location.toTuple();
+    expect(tuple).toEqual([40.7128, -74.006]);
+  });
+
+  it("should handle edge cases for coordinates", () => {
+    const edgeCaseArgs: LocationArgs = { lat: 0, lng: 0 };
+    const location = new LocationVO(edgeCaseArgs);
+
+    expect(location.lat).toBe(0);
+    expect(location.lng).toBe(0);
+  });
+});
